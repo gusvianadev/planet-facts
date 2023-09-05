dff90380f5e3e7746f411678699bd7da172d0a19
gusvianadev <gusvianadev@gmail.com>
2023-09-04 21:29:52 -0300

    redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
 	integrations: [prefetch(), tailwind()],
 	redirects: {
-		"/": "/earth",
+		"/": "/earth/overview",
 	},
 });
