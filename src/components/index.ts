bbec8a90d757802779b1a06c8bfb1012dd4acbb8
gusvianadev <gusvianadev@gmail.com>
2023-09-08 16:32:42 -0300

    finished general page

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
-// @ts-ignore: next-line
+// @ts-nocheck
 export { default as Navbar } from "./Navbar.astro";
+export { default as TabsMobile } from "./TabsMobile.astro";
+export { default as Tabs } from "./Tabs.astro";
+export { default as Planet } from "./Planet.astro";
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,9 @@ module.exports = {
 			backgroundColor: {
 				base: "var(--base)",
 			},
+			scale: {
+				"surface-base": "0.40",
+			},
 		},
 	},
 	plugins: [require("daisyui")],
